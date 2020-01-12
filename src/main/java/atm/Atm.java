2b68ed6e0f613fca8676a7929e5099dcebbc4b13
kellivalverde <paughka@gmail.comls>
2020-01-12 15:11:16 -0500

    added stretch task to comments (to do)

--- src/main/java/atm/Atm.java
+++ src/main/java/atm/Atm.java
@@ -4,7 +4,7 @@ public class Atm {
 
 	private double balance;
 	private String requiredPin = "1234";
-
+//Stretch Task: use java.math.BigDecimal to store balance instead of double.
 		
 	public Atm(double balance, String requiredPin) {
 		this.balance = balance;
